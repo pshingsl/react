2525ba6d64397f5fc454a51ce4a97f2131ba8246
pshingsl <w455ok@gmail.com>
2025-06-27 02:21:32 +0900

    복습

---
+++ front/my-app/src/components/AllDays/Day.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BottomPart, DayWrapper, TopPart } from './styles'
+
+const Day = () => {
+    return (
+        <DayWrapper>
+            <TopPart>
+                <div>
+                    <h2>wed</h2>
+                    <h3>06.25</h3>
+                </div>
+                <img src="./images/weatherIcons/snow.svg" alt=""></img>
+            </TopPart>
+            <BottomPart>
+                <h1>21</h1>
+                <h3>06.25</h3>
+            </BottomPart>
+        </DayWrapper>
+    )
+}
+
+export default Day
